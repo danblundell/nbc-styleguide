5d6286cc100465a4ef143e6844957d4072ede7db
Dan Blundell <danblundell@me.com>
2012-09-11 13:34:49 +0100

    initial commit

---
+++ js/site.js
@@ -0,0 +1,10 @@
+(function($) {
+
+	var nbcApp = nbcApp || {};
+
+	nbcApp.init = function(){
+		//onload stuff goes here - console comments break in IE 
+		//console.log("running");
+	}();
+
+})(jQuery);
